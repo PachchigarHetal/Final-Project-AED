bf80bf9239ac4bdec4f3c2c400850610983e1e02
Japan Gor <gor.j@northeastern.edu>
2022-12-10 15:05:45 -0500

    Vaccine and test added

---
+++ src/Business/Enterprise/TestingLab.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author japangor
+ */
+public class TestingLab extends Enterprise {
+    public TestingLab (String name){
+        super( EnterpriseType.TestingLab, name);
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+}
---
+++ src/Business/Enterprise/VaccineManufacturer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+
+import Business.Role.Role;
+import Business.Vaccine.VaccineDirectory;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author japangor
+ */
+public class VaccineManufacturer extends Enterprise {
+    VaccineDirectory vaccineDirectory=new VaccineDirectory();
+     
+    public VaccineManufacturer (String name){
+        super( Enterprise.EnterpriseType.VaccineManufacturer, name);
+        vaccineDirectory = new VaccineDirectory();
+    }
+
+    public VaccineDirectory getVaccineDirectory() {
+        return vaccineDirectory;
+    }
+
+    public void setVaccineDitrectory(VaccineDirectory vaccineDitrectory) {
+        this.vaccineDirectory = vaccineDitrectory;
+    }
+     
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+}
