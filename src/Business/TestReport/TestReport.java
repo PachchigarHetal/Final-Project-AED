969e72198cc3c6ba27cd2183f1ff4b0d40e4cfa3
PachchigarHetal <114840001+PachchigarHetal@users.noreply.github.com>
2022-12-11 21:28:59 -0500

    Create TestReport

---
+++ src/Business/TestReport/TestReport.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.TestReport;
+
+import Business.Person.Person;
+import java.util.Date;
+
+/**
+ *
+ * @author hetalp
+ */
+public class TestReport {
+    
+//     private Person person;
+    private boolean result;
+
+//    public Person getPerson() {
+//        return person;
+//    }
+//
+//    public void setPerson(Person person) {
+//        this.person = person;
+//    }
+
+    
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+    
+    public TestReport( Boolean result){
+        this.result = result;
+    }
+    
+}
