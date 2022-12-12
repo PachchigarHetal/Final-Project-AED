000605574d4195c27ee78c20394181424c727272
Japan Gor <gor.j@northeastern.edu>
2022-12-11 22:17:18 -0500

    Create hospital staff role

---
+++ src/Business/Role/HospitalStaffRole.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.City.City;
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.Hospital.HospitalStaffJPanel;
+import userinterface.VaccineManufacturer.ManageDeliveryManagerJPanel;
+
+/**
+ *
+ * @author japangor
+ */
+public class HospitalStaffRole extends Role {
+    private EcoSystem system;
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, City city, UserAccount userAccount, Organization organization, 
+            Enterprise enterprise, EcoSystem system) {
+        return new HospitalStaffJPanel(userProcessContainer,city, userAccount, organization, enterprise, system);
+    } 
+}
