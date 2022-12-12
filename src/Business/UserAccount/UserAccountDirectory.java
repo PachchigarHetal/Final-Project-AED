67967679ec625a2bc7b1405c3ec759bcca9e1dd0
PachchigarHetal <114840001+PachchigarHetal@users.noreply.github.com>
2022-12-11 21:29:28 -0500

    Create UserAccountDirectory

---
+++ src/Business/UserAccount/UserAccountDirectory.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.UserAccount;
+
+import Business.Employee.Employee;
+import Business.Person.Person;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hetalp
+ */
+public class UserAccountDirectory {
+    
+    private ArrayList<UserAccount> userAccountList;
+
+    public UserAccountDirectory() {
+        userAccountList = new ArrayList();
+    }
+
+    public ArrayList<UserAccount> getUserAccountList() {
+        return userAccountList;
+    }
+    
+    public UserAccount authenticateUser(String username, String password){
+        for (UserAccount ua : userAccountList)
+            if (ua.getUsername().equals(username) && ua.getPassword().equals(password)){
+                return ua;
+            }
+        return null;
+    }
+    
+    public UserAccount createUserAccount(String username, String password, Employee employee, Role role){
+        UserAccount userAccount = new UserAccount();
+        userAccount.setUsername(username);
+        userAccount.setPassword(password);
+        userAccount.setEmployee(employee);
+        userAccount.setRole(role);
+        userAccountList.add(userAccount);
+        return userAccount;
+    }
+    
+    public boolean checkIfUsernameIsUnique(String username){
+        for (UserAccount ua : userAccountList){
+            if (ua.getUsername().equals(username))
+                return false;
+        }
+        return true;
+    }
+    public void deleteUserAccount(UserAccount account){
+        userAccountList.remove(account);
+    }
+    
+     public void updateUserAccount(UserAccount userAccount, String username, String password){
+        userAccount.setUsername(username);
+        userAccount.setPassword(password);
+    }
+}
