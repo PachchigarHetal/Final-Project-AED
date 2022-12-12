d56e1fcca09596f7672d619fd5acb6347a43ee0b
PachchigarHetal <114840001+PachchigarHetal@users.noreply.github.com>
2022-12-11 21:27:24 -0500

    create org

---
+++ src/Business/Organization/Organization.java
@@ -0,0 +1,108 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization;
+
+import Business.Employee.EmployeeDirectory;
+import Business.Role.Role;
+import Business.UserAccount.UserAccountDirectory;
+import Business.WorkQueue.WorkQueue;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hetalp
+ */
+public abstract class Organization {
+
+    private String name;
+    private String type;
+    private WorkQueue workQueue;
+    private EmployeeDirectory employeeDirectory;
+    private UserAccountDirectory userAccountDirectory;
+    private int organizationID;
+    private static int counter=0;
+    
+    public enum Type{
+        DeliveryManager("Delivery Managers"),
+        Doctor("Doctor"),
+        HospitalAdmin("Hospital Admin"),
+        HospitalStaff("Hospital Staff"),
+        TestingLabAdmin("Testing Lab Admin"),
+        VaccinationCenterAdmin("Vaccination Center Admin"), 
+        VaccinationCenterStaff("Vaccination Center Staff"), 
+        VaccineManufacturerAdmin("Vaccine Manufacturer Admin"), 
+        Patient("Patient"),
+        //Person("Person"),
+        SystemAdmin("System Admin");
+        
+        private final String value;
+        
+        private Type(String value) {
+            this.value = value;
+        }
+        
+        public String getValue() {
+            return value;
+        }
+    }
+
+    public Organization(String type, String name) {
+        this.type = type;
+        this.name = name;
+        workQueue = new WorkQueue();
+        employeeDirectory = new EmployeeDirectory();
+        userAccountDirectory = new UserAccountDirectory();
+        organizationID = counter;
+        ++counter;
+    }
+    public Organization(){
+        
+    }
+    public abstract ArrayList<Role> getSupportedRole();
+    
+    public UserAccountDirectory getUserAccountDirectory() {
+        return userAccountDirectory;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+
+    public int getOrganizationID() {
+        return organizationID;
+    }
+
+    public EmployeeDirectory getEmployeeDirectory() {
+        return employeeDirectory;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWorkQueue(WorkQueue workQueue) {
+        this.workQueue = workQueue;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+    
+}
